96d026d8d65075f7b6045161117ac252ce4ba328
madhur tajane <madhurtajane@gmail.com>
2021-08-31 07:37:25 +0530

    first commit

---
+++ src/app/components/githubdemo/githubdemo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-githubdemo',
+  templateUrl: './githubdemo.component.html',
+  styleUrls: ['./githubdemo.component.css']
+})
+export class GithubdemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
